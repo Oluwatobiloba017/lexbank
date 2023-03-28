02278c48ccb84ffa414dd2187165cc6508819f47
26255734@students.lincoln.ac.uk <oluwatobiogungbangbe@gmail.com>
2023-03-28 22:42:15 +0100

    removed unwanted code from homepage

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -1,113 +1,26 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection --}}
-
 <!DOCTYPE html> 
 <html lang="en">
 	<head>
-
 		<meta charset="utf-8">
-		<title>Mentoring</title>
+		<title>LexBank</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-		
 		<!-- Favicon -->
 		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
-		
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="{{asset('asset/css/bootstrap.min.css')}}">
-		
 		<!-- Fontawesome CSS -->
 		<link rel="stylesheet" href="{{asset('asset/plugins/fontawesome/css/fontawesome.min.css')}}">
 		<link rel="stylesheet" href="{{asset('asset/plugins/fontawesome/css/all.min.css')}}">
-		
 		<!-- Main CSS -->
 		<link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
-		
 	</head>
 	<body class="account-page">
 
 		<!-- Main Wrapper -->
 		<div class="main-wrapper">
-		
-			
 			<!-- Page Content -->
 			<div class="bg-pattern-style bg-pattern-style-register">
-				<div class="content">
-								
+				<div class="content">				
 					<!-- Register Content -->
 					<div class="account-content">
 						<div class="account-box">
@@ -116,7 +29,6 @@
 									<h3><span>LexBank</span> Register</h3>
 									<p class="text-muted">Create an account</p>
 								</div>
-								
 								<!-- Register Form -->
 								<form method="POST" action="{{ route('register') }}">
                                     @csrf
@@ -203,10 +115,8 @@
 
 			</div>		
 			<!-- /Page Content -->
-		   
 		</div>
 		<!-- /Main Wrapper -->
-	  
 		<!-- jQuery -->
 		<script src="{{asset('asset/js/jquery-3.6.0.min.js')}}"></script>
 		
@@ -215,6 +125,5 @@
 		
 		<!-- Custom JS -->
 		<script src="{{asset('asset/js/script.js')}}"></script>
-		
 	</body>
 </html>
